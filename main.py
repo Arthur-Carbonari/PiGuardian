040e806ad30337e3f6ad9adc80bbb6cab34da72c
Arthur Martins <arthurcarbonari99@gmail.com>
2023-05-30 17:47:55 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+def greet_user():
+    print("Hello! Welcome to the program.")
+
+def display_menu():
+    print("Please choose one of the following options:")
+    print("1. Option 1")
+    print("2. Option 2")
+    print("3. Option 3")
+    print("4. Option 4")
+    print("5. Option 5")
+
+def handle_option(option):
+    if option == 1:
+        print("You selected Option 1.")
+    elif option == 2:
+        print("You selected Option 2.")
+    elif option == 3:
+        print("You selected Option 3.")
+    elif option == 4:
+        print("You selected Option 4.")
+    elif option == 5:
+        print("You selected Option 5.")
+    else:
+        print("Invalid option. Please choose a valid option.")
+
+def main():
+    greet_user()
+    display_menu()
+    
+    while True:
+        try:
+            option = int(input("Enter your choice (1-5): "))
+            handle_option(option)
+            break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+    
+if __name__ == "__main__":
+    main()
