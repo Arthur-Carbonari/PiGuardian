d743da80e91b6209d643408def637658261f267c
Arthur Martins <arthurcarbonari99@gmail.com>
2023-05-31 14:37:03 +0100

    Create FlaskServer class

---
+++ FlaskServer.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+
+class FlaskServer:
+    def __init__(self):
+        app = Flask(__name__)
+        bootstrap = Bootstrap(app)
+
+        @app.route('/')
+        def index():
+            return render_template('index.html')
+
+        # Define routes
+        @app.route('/start_camera', methods=['POST'])
+        def start_camera():
+            return self.handle_start_camera()
+
+        @app.route('/stop_camera', methods=['POST'])
+        def stop_camera():
+            return self.handle_stop_camera()
+
+        @app.route('/save_file', methods=['POST'])
+        def save_file():
+            return self.handle_save_file()
+
+        self.app = app
+
+    def handle_start_camera(self):
+        # Logic for starting the camera
+        return "Camera started"
+
+    def handle_stop_camera(self):
+        # Logic for stopping the camera
+        return "Camera stopped"
+
+    def handle_save_file(self):
+        file_data = request.files['file']
+        # Logic for saving the file
+        return "File saved"
+
+    def start(self):
+        self.app.run(host='0.0.0.0', port=5000)
