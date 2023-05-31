f263b12b8e2c916f99e2973e5fbebcaa5bceac4a
Arthur Martins <arthurcarbonari99@gmail.com>
2023-05-31 15:39:46 +0100

    Flip Camera video

--- Camera.py
+++ Camera.py
@@ -4,13 +4,16 @@ from threading import Condition
 from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
-
+from libcamera import Transform
 
 class Camera:
 
     def __init__(self):
         picam2 = Picamera2()
-        picam2.configure(picam2.create_video_configuration( main={"size": (640, 480)} ))
+
+        cam_config = picam2.create_video_configuration( main={"size": (640, 480)}, transform=Transform(hflip=1, vflip=1) )
+
+        picam2.configure(cam_config)
         self.output = StreamingOutput()
         picam2.start_recording(JpegEncoder(), FileOutput(self.output))
 
