ea8163e2ba933a117d96579b7acc041449d0d71d
Arthur Martins <arthurcarbonari99@gmail.com>
2023-06-05 11:26:42 -0300

    Add a main to flaskserver for debbuging purpouses

--- FlaskServer.py
+++ FlaskServer.py
@@ -43,4 +43,8 @@ class FlaskServer:
         return "File saved"
 
     def start(self):
-        self.app.run(host='0.0.0.0', port=5000)
+        self.app.run(host='0.0.0.0', port=5000, debug=True)
+
+if __name__ == "__main__":
+    fs = FlaskServer({})
+    fs.start()
