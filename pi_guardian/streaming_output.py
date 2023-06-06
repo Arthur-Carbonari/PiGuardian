53e6433e18eff1f51359d1e5b8ff9f5b85324d9c
Arthur Martins <arthurcarbonari99@gmail.com>
2023-06-06 13:11:50 -0300

    Move StreamingOutput to its own file

---
+++ pi_guardian/streaming_output.py
@@ -0,0 +1,14 @@
+from io import BufferedIOBase
+from threading import Condition
+
+
+class StreamingOutput(BufferedIOBase):
+    def __init__(self):
+        self.frame = None
+        self.condition = Condition()
+
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
+            
