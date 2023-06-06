cf700da85508d89e2311d1758d1d67282e5ef9ef
Arthur Martins <arthurcarbonari99@gmail.com>
2023-06-06 13:13:53 -0300

    Implement PiGuardian class

--- pi_guardian/pi_guardian.py
+++ pi_guardian/pi_guardian.py
@@ -1,3 +1,20 @@
+from pi_guardian.mock_camera import MockCamera
+
 
 class PiGuardian:
-    pass
+
+    def __init__(self, debug=False) -> None:
+        self.camera = MockCamera()
+
+        self.camera.start_capture()
+
+    def generate_stream(self):
+        while True:
+            frame = self.camera.get_frame()
+
+            if frame:
+                yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+    def take_picture(self):
+        self.camera.save_frame('test')
